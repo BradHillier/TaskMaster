1126706b46d22baa577f0c84a4f1e9450db30922
BradHillier <bradhillier@stumail.viu.ca>
2023-04-03 14:20:18 -0700

    add database creation and population to setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,20 @@
 # the version of python on the cubs is python 3.9
 # the below command ensures the correct version of 
 # python is used when creating a virtual environment
-python3.9 -m venv .
+if [ ! -d "venv" ]; then
 
-# install all necessary libraries
-pip3 install -r requirements.txt
+    echo "creating virtual environment"
+    python3.9 -m venv venv
+
+    # install all necessary libraries
+    pip3 install -r requirements.txt
+else
+    echo "virtual environment already exists"
+fi
+
+echo "setting up database"
+sqlite3 database.sqlite3 '.read ./schema/users.sql'
+sqlite3 database.sqlite3 '.read ./schema/task_lists.sql'
+sqlite3 database.sqlite3 '.read ./schema/tasks.sql'
+
+
