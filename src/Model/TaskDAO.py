d94902852bc81da892847bcb331c02f3af5ff99c
BradHillier <bradhillier@stumail.viu.ca>
2023-04-05 15:10:02 -0700

    modify task data object and its assocaited serializer

--- src/Model/TaskDAO.py
+++ src/Model/TaskDAO.py
@@ -1,95 +1,181 @@
 import sqlite3
-
-from User import User
-from Task import Task
-from TaskList import TaskList
-
 from TaskSerializer import TaskSerializer
+
+
+def dict_factory(cursor, row):
+    """returns each row as a dict, with column names mapped to values
+
+    taken from
+    https://docs.python.org/3/library/sqlite3.html#sqlite3-howto-row-factory
+    """
+    fields = [column[0] for column in cursor.description]
+    return {key: value for key, value in zip(fields, row)}
 
 
 class TaskDAO:
 
-    def __init__(self, db: str):
-        self.db = db
+    def __init__(self, db_path: str):
+        """Create an instance of the TaskDAO
+
+        :param db_path: The path to the database containing a Task table
+        """
+        self.db = db_path
         self.serializer = TaskSerializer()
 
-    def create(self, task_list, **kwargs):
+    def create(self, **kwargs) -> int:
+        """Create a new Task
 
-        # default values
-        attrs = {
-            'listID': task_list.ID,
-            'username': task_list.user.username,
-            'taskName': None,    
-            'description': None, 
-            'dueDate': None,     
-            'isCompleted': False, 
-            'priority': None
+        :param listID: The ID of the TaskList the Task will be added to
+        :param username: The username of the creator of the Task
+        :param taskName: The name of the Task
+        :param description: A description of the Task
+        :param dueDate: The date the Task is due on, should be a date object
+        :param isCompleted: A boolean denoting whether the task has been completed
+        :param priority: A string representing the urgency of the Task
+        :return: The ID of the newly created Task if successful, otherwise None
+
+        Example
+        -------
+        ```python3
+        from datetime import date
+
+
+        myTaskDAO = TaskDAO('path/to/database.sqlite3')
+        task_data = {
+            'listID': 1,
+            'username': 'user1',
+            'taskName': 'my important task',
+            'description': None,
+            'dueDate': date.today(),
+            'isCompleted': False,
+            'priority': 'high'
         }
-        required = ('taskName', 'dueDate')
-
-        # update the default values with those passed by the caller
-        for key in attrs.keys():
-            if key in kwargs:
-                attrs[key] = kwargs[key]
-            elif key in required:
-                print(f'Error: missing key, "{key}" is required')
-
+        new_task_id = myTaskDAO.create(**task_data)
+        ```
+        """
+        task_data = self.serializer.serialize(req_all=True, **kwargs) 
+        import pdb; pdb.set_trace()
         with sqlite3.connect(self.db) as conn:
+            conn.row_factory = dict_factory
             cur = conn.cursor()
+            cur.execute("PRAGMA foreign_keys = 1") # enables FK constraints
             cur.execute("""
-                INSERT INTO Tasks (listID, username, taskName, description, dueDate, isCompleted, priority)
+                INSERT INTO Tasks (
+                    listID, 
+                    username, 
+                    taskName, 
+                    description,
+                    dueDate, 
+                    isCompleted, 
+                    priority)
                 VALUES (?, ?, ?, ?, ?, ?, ?)
-            """, [key for key in attrs.values()])
+            """, task_data)
             conn.commit()
+            return cur.lastrowid
 
-    def getTasks(self, task_list: TaskList):
+    def getTask(self, task_id: int) -> object:
+        """Retreive the Task with the specified ID
+
+        :param task_id: The ID of the task to be retreived
+        :return: An object created by serializing the retreived data
+
+        Example
+        -------
+        ```python3
+        my_task_dao = TaskDAO('path/to/database.sqlite3')
+        task_id = 1
+        my_task_obj = my_task_dao.getTask(task_id)
+        ```
+        """
         with sqlite3.connect(self.db) as conn:
+            conn.row_factory = dict_factory
             cur = conn.cursor()
-            res = cur.execute( """
+            res = cur.execute("""
+                SELECT *
+                FROM Tasks
+                WHERE taskID = (?)
+            """, [task_id])
+            return self.serializer.deserialize(**res.fetchone())
+
+    def getTasks(self, list_id: int) -> list:
+        """Get all the tasks contained in a specific list
+
+        The type of objects contained within the list are determined by
+        the serializer.
+
+        :param list_id: The ID of the task list for which to retreive tasks
+        :return: A list containing all Tasks within the specified list
+
+        Example
+        -------
+        ```python3
+        my_task_dao = TaskDAO('path/to/database.sqlite3')
+        task_list_id = 1
+        my_list_of_task_objs = my_task_dao.getTasks(task_id)
+        ```
+        """
+        with sqlite3.connect(self.db) as conn:
+            conn.row_factory = dict_factory
+            cur = conn.cursor()
+            res = cur.execute("""
                 SELECT *
                 FROM Tasks
                 WHERE listID = (?)
-            """, [task_list.ID])
+            """, [list_id])
+            return  [self.serializer.deserialize(**row) for (row) in res.fetchall()]
 
-            for task in res.fetchall():
-                task_list.append(self.serializer.deserialize(task))
+    def update(self, task_id: int, **kwargs):
+        """
+        :param task_id:
 
-    def update(self, task, **kwargs) -> Task:
-        pass
+        Example
+        -------
+        ```python3
+        from datetime import date, timedelta
 
-    def delete(self, task_list: TaskList, task: Task):
+
+        my_task_dao = TaskDAO('path/to/database.sqlite3')
+        my_task_id = 1
+        task_data = {
+            'description': 'detailed description of task',
+            'dueDate': date.today() + timedelta(days=3),
+        }
+        my_task_dao.update(my_task_id, **task_data)
+        ```
+
+        WARNING
+        -------
+        The SQL statement executed by this function is dynamically created. 
+        All input from the user has been sanitized, but column names provided 
+        by the developer are directly inserted into the string. They are first 
+        checked against a list of allowed keywords, but this still opens a 
+        potential security risk.
+        """
+        with sqlite3.connect(self.db) as conn:
+            cur = conn.cursor()
+
+            # stmt is constructed with params in provided order            
+            sql = 'UPDATE Tasks SET'
+            for col in kwargs.keys(): 
+                if col in self.allowed_kwargs:
+                    sql = sql + ''.join(f' {col} = (?) ')
+                else:
+                    raise KeyError(f'Invalid keyword {col}')
+            sql = sql + 'WHERE taskID = (?)'
+
+            params = [value for value in kwargs.values()]
+            params.append(task_id)
+            cur.execute(sql, params) 
+
+    def delete(self, task_id: int):
+        """Delete a Task
+
+        :param task_id: The ID of the task to delete
+        """
         with sqlite3.connect(self.db) as conn:
             cur = conn.cursor()
             cur.execute("""
                 DELETE FROM Tasks
                 WHERE taskID = (?)
-            """, [task.ID])
+            """, [task_id])
             conn.commit()
-        task_list.tasks.remove(task)
-
-
-if __name__ == '__main__':
-    myUser = User(username='test_user', email='user@example.com')
-    myList = TaskList(ID=1, name='Inbox', user=myUser) 
-
-    myDAO = TaskDAO('../../database.sqlite3')
-
-    # Create a task
-    print('checking if a task can be created')
-    task_data = {
-            'taskName': 'my test task',
-            'dueDate': '2023-04-05'
-    }
-    myDAO.create(myList, **task_data)
-
-    # check if the previously created task is in the task list
-    myDAO.getTasks(myList)
-    myTask = [task for task in myList.tasks if task.name == 'my test task']
-    assert myTask[0].name == 'my test task' 
-    assert len(myTask) == 1
-
-    # check if the previously created task was properly deleted
-    print('checking if a task can be deleted')
-    myDAO.delete(myList, myTask[0])
-    matches = [task for task in myList.tasks if task.name == 'my test task']
-    assert len(matches) == 0
--- src/Model/TaskSerializer.py
+++ src/Model/TaskSerializer.py
@@ -4,13 +4,44 @@ import dateparser
 
 class TaskSerializer:
 
-    def deserialize(self, task_data: tuple) -> Task:
+    allowed_kwargs = [
+            'listID',
+            'username',
+            'taskName',
+            'description',
+            'dueDate',
+            'isCompleted',
+            'priority']
+
+    def serialize(self, req_all=False, **kwargs):
+
+        # Check for any unexpected keywords
+        unexpected_keys = [key for key in kwargs if key not in self.allowed_kwargs]
+        if len(unexpected_keys) != 0:
+            raise TypeError(f'Unexpected keyword arguments {unexpected_keys}')
+
+        # If all keywords are required, ensure they were provided
+        if req_all == True:
+            missing_keys = [key for key in self.allowed_kwargs if key not in kwargs]
+            if len(missing_keys) != 0:
+                raise KeyError(f'Missing required keywords {missing_keys}')
+
+        if 'dueDate' in kwargs:
+            kwargs['dueDate'] = kwargs['dueDate'].isoformat()
+
+        # ensure values appear in the expected order
+        return tuple(kwargs[key] for key in self.allowed_kwargs if key in kwargs)
+
+
+    def deserialize(self, **kwargs) -> Task:
         return Task(
-            ID = task_data[0],
-            listID = task_data[1],
-            user = task_data[2],
-            name = task_data[3],
-            description = task_data[4],
-            dueDate = dateparser.parse(task_data[5]),
-            isCompleted = task_data[6],
-            priority = task_data[7])
+                ID = kwargs.get('taskID'),
+                listID = kwargs.get('listID'),
+                user = kwargs.get('username'),
+                name = kwargs.get('taskName'),
+                description = kwargs.get('description'),
+                dueDate = kwargs.get('dueDate'),
+                isCompleted = kwargs.get('isCompleted'),
+                priority = kwargs.get('priority'))
+
+
